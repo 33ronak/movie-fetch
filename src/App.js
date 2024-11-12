d690b8fd056ca2a198d27f1b005824004303fb8a
33ronak <08arnab98@gmail.com>
2024-11-12 12:00:00 +0530

    fetch functionality added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [moviesList, setMoviesList] = useState([])
+
+  const fetchMoviesHandler = () => {
+    fetch("https://swapi.dev/api/films/").then(response => {
+      return response.json();
+    }).then(data => {
+      const tranformedMovies = data.results.map(moviedata => {
+        return {
+          id: moviedata.episode_id,
+          title: moviedata.title,
+          openingText: moviedata.opening_crawl,
+          releaseDate: moviedata.release_date
+        }
+      });
+      setMoviesList(tranformedMovies);
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler} >Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={moviesList} />
+      </section>
+    </React.Fragment>
   );
 }
 
